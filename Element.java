b7a35ae483089edcb62730d204786c3e13cbf253
TheProperGlitch <bvancepantz@gmail.com>
2024-08-29 19:20:38 -0700

    Update Element.java
    ahhh

--- Element.java
+++ Element.java
@@ -1,11 +1,12 @@
 public class Element {
 
     Integer value;
-    Element next;
+    Element lchild;
+    Element rchild;
     Element prev;
     int key;
 
-    Element(Integer input){
+    Element(Integer input, int key){
         value = input;
     }
 
@@ -17,30 +18,38 @@ public class Element {
     key = input;
     }
 
-    void setting(Element input){
-        if (next == null){
-            next =input;
-            next.setPrev(this);
-        }
-        else next.setting(input);
-        
+    int getKey(){
+        return key;
     }
 
-    void printing(){
-        System.out.print(value.toString() + ", ");
-        if (!(next == null)) next.printing(); 
+    void setting(Element input){
+        if (input.getKey() > key) {
+            if (lchild == null){
+                lchild = input;
+                lchild.setPrev(this);
+            }
+            else lchild.setting(input);
+        } else {
+            if (rchild == null){
+                rchild = input;
+                rchild.setPrev(this);
+            }
+            else rchild.setting(input); 
+        }
+        
     }
 
     Element searching(int input){
         if (input == key) return this;
-        else return next.searching(input);
+        else {
+            if (input > key) return lchild.searching(input);
+            else return rchild.searching(input);
+        }
     }
 
-    Element getNext(){
-        return next;
-    }
 
     void deleting(Element input){
+        if 
         if (next == input) next = next.getNext();
         else next.deleting(input);
     }
